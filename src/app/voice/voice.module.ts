0b5de0d1a425b1758a4a393ec1c844f7aecb871a
DABAOKU <dabaoku1028@gmail.com>
2020-07-11 01:11:54 +0800

    🚧 WIP : Voice work in progress...

--- src/app/voice/voice.module.ts
+++ src/app/voice/voice.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { VoicePageRoutingModule } from './voice-routing.module';
-
 import { VoicePage } from './voice.page';
+import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
+    // BrowserModule,
     FormsModule,
     IonicModule,
-    VoicePageRoutingModule
+    VoicePageRoutingModule,
+    // BrowserAnimationsModule
   ],
-  declarations: [VoicePage]
+  declarations: [VoicePage],
 })
 export class VoicePageModule {}
--- src/app/voice/voice.page.ts
+++ src/app/voice/voice.page.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { AudioConfig, ResultReason, SpeechConfig, SpeechRecognizer } from 'microsoft-cognitiveservices-speech-sdk';
 
 @Component({
   selector: 'app-voice',
   templateUrl: './voice.page.html',
   styleUrls: ['./voice.page.scss'],
 })
-export class VoicePage implements OnInit {
+export class VoicePage{
 
-  constructor() { }
+  name = 'Speech To Text';
+  private recognizing = false;
+  private notification: string;
+  private innerHtml: string = '';
+  private lastRecognized: string = '';
+  _recognizer: SpeechRecognizer;
+  startButton(event) {
+    console.log("start")
+    
+    if (this.recognizing) {
+      this.stop();
+      this.recognizing = false;
+    }
+    else {
 
-  ngOnInit() {
+      this.recognizing = true;
+      console.log("record");
+
+      const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+
+      const speechConfig = SpeechConfig.fromSubscription("2e034ce056e9485f97b82e49898ec0e2", "eastus");
+      speechConfig.speechRecognitionLanguage = 'zh-TW';
+      speechConfig.enableDictation();
+      this._recognizer = new SpeechRecognizer(speechConfig, audioConfig)
+      this._recognizer.recognizing = this._recognizer.recognized = this.recognizerCallback.bind(this)
+      this._recognizer.startContinuousRecognitionAsync();
+    }
+  }
+
+  recognizerCallback(s, e) {
+    console.log(e.result.text);
+    const reason = ResultReason[e.result.reason];
+    console.log(reason);
+    if (reason == "RecognizingSpeech") {
+      this.innerHtml = this.lastRecognized + e.result.text;
+    }
+    if (reason == "RecognizedSpeech") {
+      this.lastRecognized += e.result.text + "\r\n";
+      this.innerHtml = this.lastRecognized;
+    }
+  }
+
+  stop() {
+    this._recognizer.stopContinuousRecognitionAsync(
+      stopRecognizer.bind(this),
+      function (err) {
+        stopRecognizer.bind(this)
+        console.error(err)
+      }.bind(this)
+    )
+
+    function stopRecognizer() {
+      this._recognizer.close()
+      this._recognizer = undefined
+      console.log('stopped')
+    }
   }
 
 }
