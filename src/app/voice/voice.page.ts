2759398125c9a4d1f3345abdb58f3754d4d9c7ee
dabaoku <dabaoku@gmail.com>
2020-08-02 22:18:38 +0800

    ✅ feat(ui): Voice Recgonize function

--- src/app/voice/voice.page.ts
+++ src/app/voice/voice.page.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AudioConfig, ResultReason, SpeechConfig, SpeechRecognizer } from 'microsoft-cognitiveservices-speech-sdk';
 import { GetAllIngredientService } from '../services/get-all-ingredient.service';
+import { never } from 'rxjs';
+import { NavController } from '@ionic/angular';
+
+// interface resultArray {
+//   id: number;
+//   name: string;
+// }
 
 @Component({
   selector: 'app-voice',
@@ -9,73 +16,112 @@ import { GetAllIngredientService } from '../services/get-all-ingredient.service'
 })
 export class VoicePage{
 
-  constructor(private getAllIngredientService: GetAllIngredientService) {
-  }
+  startRecognize: boolean;
+  allIngredient;
+
 
   name = 'Speech To Text';
   private recognizing = false;
-  private notification: string;
-  innerHtml: string = '';
+  // innerHtml: string = '';
   private lastRecognized: string = '';
   _recognizer: SpeechRecognizer;
-  startButton(event) {
-    console.log("start")
-    
-    if (this.recognizing) {
-      this.stop();
-      this.recognizing = false;
-    }
-    else {
 
-      this.recognizing = true;
-      console.log("record");
+  constructor(private getAllIngredientService: GetAllIngredientService, private navCtrl: NavController) {
+  }
 
-      const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+  start() {
 
-      const speechConfig = SpeechConfig.fromSubscription("2e034ce056e9485f97b82e49898ec0e2", "eastus");
-      speechConfig.speechRecognitionLanguage = 'zh-TW';
-      speechConfig.enableDictation();
-      this._recognizer = new SpeechRecognizer(speechConfig, audioConfig)
-      this._recognizer.recognizing = this._recognizer.recognized = this.recognizerCallback.bind(this)
-      this._recognizer.startContinuousRecognitionAsync();
-    }
+    // 先呼叫Ingredient API，不然太大包
+    this.getAllIngredientService.request().subscribe( data =>{
+      this.allIngredient = data;
+     });
+
+    console.log('start');
+    this.startRecognize = true; // 用來判斷是否hidden
+
+    this.recognizing = true;
+
+    const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+
+    const speechConfig = SpeechConfig.fromSubscription('2e034ce056e9485f97b82e49898ec0e2', 'eastus');
+    speechConfig.speechRecognitionLanguage = 'zh-TW';
+    speechConfig.enableDictation();
+    this._recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+    this._recognizer.recognizing = this._recognizer.recognized = this.recognizerCallback.bind(this);
+    this._recognizer.startContinuousRecognitionAsync();
   }
 
   recognizerCallback(s, e) {
-    console.log(e.result.text);
+    // console.log(e.result.text);
     const reason = ResultReason[e.result.reason];
-    console.log(reason);
-    if (reason == "RecognizingSpeech") {
-      this.innerHtml = this.lastRecognized + e.result.text;
-    }
-    if (reason == "RecognizedSpeech") {
-      this.lastRecognized += e.result.text + "\r\n";
-      this.innerHtml = this.lastRecognized;
+    // console.log(reason);
+    // if(reason === 'RecognizingSpeech') {
+    //   this.innerHtml = this.lastRecognized + e.result.text;
+    // }
+    if (reason === 'RecognizedSpeech') {
+      this.lastRecognized += e.result.text + '\r\n';
+      // this.innerHtml = this.lastRecognized;
+      console.log(this.lastRecognized);
     }
   }
 
   stop() {
+    this.startRecognize = false; // 用來判斷是否hidden
+    this.recognizing = false;
+
     this._recognizer.stopContinuousRecognitionAsync(
       stopRecognizer.bind(this),
-      function (err) {
-        stopRecognizer.bind(this)
-        console.error(err)
+      function(err) {
+        stopRecognizer.bind(this);
+        console.error(err);
       }.bind(this)
-
-    )
+    );
 
     function stopRecognizer() {
-      this._recognizer.close()
-      this._recognizer = undefined
-      console.log('stopped')
+      this._recognizer.close();
+      this._recognizer = undefined;
+      console.log('stopped');
     }
+
+    // 做語句分析配對食材
+    this.recognizeAllIngredient();
   }
 
-  showAllIngredient(){
-    this.getAllIngredientService.request().subscribe( data =>{
-     console.log(data);
-    })
+  recognizeAllIngredient() {
+    localStorage.removeItem('voiceResult');
+    localStorage.removeItem('voiceResultName');
+    let voiceResultID = [null];
+    let voiceResultName = [null];
+    let j = 0;
+
+    // 與資料庫做比對
+    for (let i = 0 ; i <= this.allIngredient.response.data.length ; i++) {
+      if (this.lastRecognized.includes(this.allIngredient.response.data[i]?.ingredient_name)) {
+        voiceResultID[j] = (this.allIngredient.response.data[i].ingredient_id);
+        voiceResultName[j] = (this.allIngredient.response.data[i].ingredient_name);
+        j++;
+       } else if ((this.allIngredient.response.data[i]?.ingredient_alias_1 !== null) &&
+        (this.lastRecognized.includes(this.allIngredient.response.data[i]?.ingredient_alias_1))) {
+        voiceResultID[j] = (this.allIngredient.response.data[i].ingredient_id);
+        voiceResultName[j] = (this.allIngredient.response.data[i].ingredient_name);
+        j++;
+       } else if ((this.allIngredient.response.data[i]?.ingredient_alias_2 !== null) &&
+       (this.lastRecognized.includes(this.allIngredient.response.data[i]?.ingredient_alias_2))) {
+        voiceResultID[j] = (this.allIngredient.response.data[i].ingredient_id);
+        voiceResultName[j] = (this.allIngredient.response.data[i].ingredient_name);
+        j++;
+      } else if ((this.allIngredient.response.data[i]?.ingredient_alias_3 !== null) &&
+      (this.lastRecognized.includes(this.allIngredient.response.data[i]?.ingredient_alias_3))) {
+        voiceResultID[j] = (this.allIngredient.response.data[i].ingredient_id);
+        voiceResultName[j] = (this.allIngredient.response.data[i].ingredient_name);
+        j++;
+     }
+    }
+
+    console.log(voiceResultID);
+    console.log(voiceResultName);
+    localStorage.setItem('voiceResultID', voiceResultID.toString());
+    localStorage.setItem('voiceResultName', voiceResultName.toString());
+    this.navCtrl.navigateBack('main');
   }
-
-
 }
