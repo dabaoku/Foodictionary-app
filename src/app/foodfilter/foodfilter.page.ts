ae30c7719c73bd5e68813eb7c5bc39f5d7840530
Hans Sung (ManpowerGroup Taiwan) <v-hsung@microsoft.com>
2020-09-14 21:50:11 +0800

    add capacitor camera

--- src/app/foodfilter/foodfilter.page.ts
+++ src/app/foodfilter/foodfilter.page.ts
@@ -1,61 +1,94 @@
-import { Component, OnInit } from '@angular/core';
-import { FoodFilterUrlService } from './food-filter-url.service';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { FoodFilterUrlService } from "./food-filter-url.service";
+import { NavController } from "@ionic/angular";
+import {
+    Plugins,
+    CameraResultType,
+    Capacitor,
+    FilesystemDirectory,
+    CameraPhoto,
+    CameraSource,
+} from "@capacitor/core";
+import { SafeResourceUrl, DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-foodfilter',
-  templateUrl: './foodfilter.page.html',
-  styleUrls: ['./foodfilter.page.scss'],
+    selector: "app-foodfilter",
+    templateUrl: "./foodfilter.page.html",
+    styleUrls: ["./foodfilter.page.scss"],
 })
 export class FoodfilterPage implements OnInit {
-  url: string;
-  detectOutcome: any;
-  tagName = [];
-  newTagName: any;
+    url: string;
+    detectOutcome: any;
+    tagName = [];
+    newTagName: any;
+    image: SafeResourceUrl;
 
-  constructor(
-    private foodFilterUrlService: FoodFilterUrlService, private navCtrl: NavController) { }
+    constructor(
+        private foodFilterUrlService: FoodFilterUrlService,
+        private navCtrl: NavController,
+        private domSanitizer: DomSanitizer
+    ) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
-  submit() {
-    this.foodFilterUrlService
-    .request(this.url) 
-    .subscribe((data) => {
-      this.detectOutcome = data.predictions;
-      console.log('ts data:', data);
-      console.log('ts detectOutcome:', this.detectOutcome);
-      console.log('ts detectOutcome length:', this.detectOutcome.length);
+    submit() {
+        this.foodFilterUrlService.request(this.url).subscribe((data) => {
+            this.detectOutcome = data.predictions;
+            console.log("ts data:", data);
+            console.log("ts detectOutcome:", this.detectOutcome);
+            console.log("ts detectOutcome length:", this.detectOutcome.length);
 
-      for (let i = 0, length = this.detectOutcome.length; i < length; i++) {
-        if (this.detectOutcome[i].probability >= 0.7) {
-          console.log('1',this.detectOutcome[i].tagName);
-          // this.tagName = this.detectOutcome[i].tagName;
-          this.tagName.push(this.detectOutcome[i].tagName);
-          // this.tagName[i].push(this.detectOutcome[i].tagName);
-          // console.log(this.tagName[i]);
-        }
-      }
-      console.log('push過的tagName', this.tagName);
-      console.log('2', Array.from(new Set(this.tagName)));
-      console.log('push且經過set過的tagName', this.tagName);
+            for (
+                let i = 0, length = this.detectOutcome.length;
+                i < length;
+                i++
+            ) {
+                if (this.detectOutcome[i].probability >= 0.7) {
+                    console.log("1", this.detectOutcome[i].tagName);
+                    // this.tagName = this.detectOutcome[i].tagName;
+                    this.tagName.push(this.detectOutcome[i].tagName);
+                    // this.tagName[i].push(this.detectOutcome[i].tagName);
+                    // console.log(this.tagName[i]);
+                }
+            }
+            console.log("push過的tagName", this.tagName);
+            console.log("2", Array.from(new Set(this.tagName)));
+            console.log("push且經過set過的tagName", this.tagName);
 
-      this.newTagName = Array.from(new Set(this.tagName));
-      console.log('newTagName' , this.newTagName);
-      console.log('newTagName toString' , this.newTagName.toString());
+            this.newTagName = Array.from(new Set(this.tagName));
+            console.log("newTagName", this.newTagName);
+            console.log("newTagName toString", this.newTagName.toString());
+        });
+    }
 
-    });
-  }
+    //還要寫動態刪除辨識錯誤的食材
 
-  //還要寫動態刪除辨識錯誤的食材
+    SentDetectOutcome() {
+        localStorage.setItem("newTagName", this.newTagName.toString());
 
-  SentDetectOutcome() {
-    localStorage.setItem('newTagName', this.newTagName.toString());
+        this.navCtrl.navigateRoot("main").then(() => {
+            window.location.reload();
+        });
+    }
 
-    this.navCtrl.navigateRoot('main').then(() => {
-      window.location.reload();
-    });
-  }
+    public async addNewToGallery() {
+        const { Camera, Filesystem, Storage } = Plugins;
 
+        // Take a photo
+        const capturedPhoto = await Camera.getPhoto({
+            resultType: CameraResultType.Base64,
+            source: CameraSource.Camera,
+            quality: 100,
+            allowEditing: true,
+        });
+        console.log(
+            "this is capturedPhoto: ",
+            capturedPhoto,
+            capturedPhoto.base64String
+        );
+        this.image = this.domSanitizer.bypassSecurityTrustResourceUrl(
+            capturedPhoto && capturedPhoto.base64String
+        );
+        console.log(this.image);
+    }
 }
